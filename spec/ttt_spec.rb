4d2aac65d12a80030b1608cf522037f49e500560
craigs40 <craig.shay40@gmail.com>
2021-03-25 12:44:55 -0400

    Update negative test cases

--- spec/ttt_spec.rb
+++ spec/ttt_spec.rb
@@ -3,26 +3,26 @@ require_relative '../lib/board'
 require_relative '../bin/game'
 
 RSpec.describe Player do
-  let!(:p1_array) { [] }
-  let!(:p2_array) { [] }
-  describe 'initialize' do
-    it 'initializes player variables' do
-      expect(@p1_array).to eq(nil)
+  let!(:p1_array) { [1, 2, 3] }
+  let!(:p2_array) { [4, 5, 6] }
+  describe '#initialize' do
+    it 'initializes player array' do
+      expect(@p1_array).to eq([1, 2, 3])
     end
-    it 'does not add ' do
-      expect(@p2_array).to eq(nil)
+    it 'does not add letters to the array' do
+      expect(@p2_array).not_to eq([a, b, c])
     end
   end
 end
 
 RSpec.describe Board do
   let!(:board) { Board.new }
-  describe 'board' do
+  describe '#board' do
     it 'initializes the board' do
       expect(@board).to eq(nil)
     end
-    it 'does not print the board' do
-      expect(@board).to eq(nil)
+    it 'does not display the player symbol' do
+      expect(@board).not_to eq([x])
     end
   end
 end
@@ -31,5 +31,8 @@ RSpec.describe '#display' do
   it 'should display empty cells' do
     board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     expect(display(board)).to start_with('0 | 1 | 2')
+  end
+  it 'should not change board layout' do
+    expect(display(board)).not_to start_with('4 | 5 | 6')
   end
 end
